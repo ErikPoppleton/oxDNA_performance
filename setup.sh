77c12756c05a3e98732f2edb4a57f7055f4521fe
ErikPoppleton <rkpoppleton@gmail.com>
2023-06-16 15:04:58 +0200

    better submit files

--- setup.sh
+++ setup.sh
@@ -1,9 +1,11 @@
 for x in 32768 4096 512 64 8; do
+    cp submit.sh N$x
+    sed -i "s/strands/$x/g" N$x/submit.sh
     for y in 2 5 10 20 40; do
 	mkdir N$x\M$y
 	cp submit_mps.sh N$x\M$y
-	sed -i "s/strands/$n/g" N$x\M$y\/submit_mps.sh
-	sed -i "s/proc/$m/g" N$x\M$y\/submit_mps.sh
+	sed -i "s/strands/$x/g" N$x\M$y\/submit_mps.sh
+	sed -i "s/proc/$y/g" N$x\M$y\/submit_mps.sh
 	if [[ $y -eq 20 ]] || [[ $y -eq 40 ]]; then # you may not need this.
 	    sed -i "s/-c $y/-c 16/g" N$x\M$y\/submit_mps.sh
 	fi
--- submit.sh
+++ submit.sh
@@ -1,16 +1,14 @@
 #!/bin/bash -l
 # Standard output and error:
-#SBATCH -o ./job.%j.out
-#SBATCH -e ./job.%j.err
-# Initial working directory:
+#SBATCH -o slurm.%j.out
+#SBATCH -e slurm.%j.err
 # Job name
-#SBATCH -J speed
-#
+#SBATCH -J Nstrands
+# Resources
 #SBATCH -N 1
 #SBATCH -n 1
 #SBATCH -c 1
 #SBATCH --constraint="gpu"
-#
 #SBATCH --gres=gpu:a100:1
 #SBATCH --mem=10000
 #
@@ -19,9 +17,6 @@
 #SBATCH --time=1-00:00:00
 
 module purge
-module load anaconda/3
 module load cuda/11.4
-
-conda activate oxDNA
 
 ~/software/oxDNA/build/bin/oxDNA input
